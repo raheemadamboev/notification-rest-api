c0882e6f58a96ff51a48c860902152f48d8e71e4
Raheem <raheemideadev@gmail.com>
2021-11-06 21:28:05 +0500

    send notification route

--- src/main/kotlin/xyz/teamgravity/plugins/Routing.kt
+++ src/main/kotlin/xyz/teamgravity/plugins/Routing.kt
@@ -1,16 +1,34 @@
 package xyz.teamgravity.plugins
 
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import xyz.teamgravity.data.remote.OneSignalService
+import xyz.teamgravity.data.remote.dto.NotificationDto
+import xyz.teamgravity.data.remote.dto.NotificationMessageDto
 
-fun Application.configureRouting() {
+fun Application.configureRouting(service: OneSignalService) {
 
     routing {
-        get("/") {
-                call.respondText("Hello World!")
+        get("/sendNotification") {
+            val title = call.parameters["title"] ?: "title"
+            val description = call.parameters["description"] ?: "description"
+
+            val successful = service.sendNotification(
+                NotificationDto(
+                    includedSegments = listOf("All"),
+                    headings = NotificationMessageDto(en = title),
+                    contents = NotificationMessageDto(en = description),
+                    appId = OneSignalService.ONE_SIGNAL_APP_ID
+                )
+            )
+
+            if (successful) {
+                call.respond(HttpStatusCode.OK)
+            } else {
+                call.respond(HttpStatusCode.InternalServerError)
             }
+        }
     }
 }
